b84f60092f43cd4db7109729eac2c0b0de8cb27b
rainbowlotus-games <rainbowlotus.games@gmail.com>
2021-03-19 06:09:28 +0100

    Add color blending

--- Assets/Scripts/HexMap/HexCell.cs
+++ Assets/Scripts/HexMap/HexCell.cs
@@ -8,6 +8,20 @@ namespace HexMap
     {
         public HexCoordinates coordinates;
 
+        [SerializeField]
+        HexCell[] neighbors;
+
         public Color color;
+
+        public HexCell GetNeighbor(HexDirection direction)
+        {
+            return neighbors[(int)direction];
+        }
+
+        public void SetNeighbor(HexDirection direction, HexCell cell)
+        {
+            neighbors[(int)direction] = cell;
+            cell.neighbors[(int)direction.Opposite()] = this;
+        }
     }
 }
